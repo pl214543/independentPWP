7a6bda514602d453547c03bf33239546760a5774
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-01-25 08:33:46 -0500

    Update canCircles.py

--- can/canCircles.py
+++ can/canCircles.py
@@ -1,31 +1,30 @@
-# Set imread of opencv to the downloaded image
-# using canny edge, hough, etc.
-# get a fixed point in the center
-
 # required libraries
 import cv2
 import numpy as num
 
 # image name so that the image can be taken from pycharm project files.
-# might need to be changed to a path if using terminal
 imageName = "can2.png"
+
+# image name so that a new image can be saved to a new file and not overwrite the original
 newImageName = "canCircle.jpg"
 
-# defines the image needed
+# defines the image needed using cv2
 image = cv2.imread(imageName, cv2.IMREAD_COLOR)
 
 # makes the image grey for easier detection
 grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # blur the grey image
-blurred = cv2.blur(grey, (1, 1))
+blurred = cv2.GaussianBlur(grey, (1, 1), 0)
 
 # employs the use of hough circles (cv2 predefined method)
+# makes the minRadius and maxRadius equal to 0 so that they have no limit and detect the largest circle (outer circle)
 houghCircle = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT,
                                1, 20, param1=50, param2=30, minRadius=0,
                                maxRadius=0)
 
-# if the circle created exists, common error when nothing is detected as none, so it flags errors
+# if the circle created exists, common error when nothing is detected as none, so it prevents this
+# if nothing is detected or created (maybe too blurred), then nothing will happen
 if houghCircle is not None:
 
     # rounds the numbers to an unassigned long of 16 using numpy
@@ -47,6 +46,7 @@ if houghCircle is not None:
         # displays the new image with overlay in a window named "Circle"
         cv2.imshow("Circle", image)
 
+        # saves the image to file name "canCircle.jpg"
         cv2.imwrite(newImageName, image)
 
         # image closes after any key is pressed
