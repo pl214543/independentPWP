25aff984f3cafa02e604f00dd96607a618e666bc
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-02-04 16:54:22 -0500

    Update finalVideo.py

--- rollingCan/finalVideo.py
+++ rollingCan/finalVideo.py
@@ -1,1 +1,24 @@
+# import required libraries
+import cv2
+import numpy as num
 
+# imports the final array of edited frames
+from houghWriting import newFrames
+
+# gets the height and width of the original frames for the video
+height,width,layers=newFrames[1].shape
+
+# sets up an MP4 video file for saving named "finalVideo.mp4"
+fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+finalVideo = cv2.VideoWriter('finalVideo.mp4', fourcc, 20.0, (width, height))
+
+# every frame in the final array
+for frame in newFrames:
+    frameFinal = frame
+
+    # writes each frame to the video
+    finalVideo.write(frameFinal)
+
+# finally closes everything
+cv2.destroyAllWindows()
+finalVideo.release()
