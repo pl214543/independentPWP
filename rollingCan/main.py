00471d032dfda0cc24be7b1d8d85b7695db353d8
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-02-04 16:53:24 -0500

    Update main.py

--- rollingCan/main.py
+++ rollingCan/main.py
@@ -8,59 +8,15 @@
 import cv2
 import numpy as num
 
-frames = []
-
+# file name
 recording = "IMG_1791.mp4"
 
+# the video capture is set up for opencv2
 video = cv2.VideoCapture(recording)
 
+# checks if the video is active, and if it is not then it prints an error message
 if video.isOpened() == False:
     print("Error opening video.")
 
-# this is for shuffling through the frames
-currentFrame = video.get(cv2.CAP_PROP_POS_FRAMES)
-
-# runs ony when the video capture is open
-while (video.isOpened()):
-    booleanReady, frame = video.read()
-
-    print(type(frame))
-    print(frame)
-
-    if booleanReady == True:
-
-        # this appends this the frame to the numpy array
-        frames.append(frame)
-
-        # gets the next frame ready
-        currentFrame = video.get(cv2.CAP_PROP_POS_FRAMES)
-
-    else:
-
-        # in case the previous frame didn't load, it goes back to it
-        video.set(cv2.CAP_PROP_POS_FRAMES, currentFrame-1)
-
-        # gives it a little time to load
-        cv2.waitKey(1000)
-
-    # checks if the current frame is the last frame
-    if video.get(cv2.CAP_PROP_POS_FRAMES) == video.get(cv2.CAP_PROP_FRAME_COUNT):
-
-        # stop
-        break
-
-print("\n\n\n\n\n\n\n\n\nTest\n\n\n\n\n\n\n\n")
-print(len(frames))
-print(frames[45])
-video = cv2.imwrite("video.jpg", frames[90])
-
-    # # makes it grayscale so that the hough circles method works
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # # blurring the video so that the can is more prominent than the background
-    # blurred = cv2.GaussianBlur(gray, (3, 3), 0)
-    # # median blurring
-    # median = cv2.medianBlur(blurred, 5)
-    # # uses hough circle on the most recent version of the video
-    # houghCircle = cv2.HoughCircles(median, cv2.HOUGH_GRADIENT,
-    #                                1, 20, param1=50, param2=30, minRadius=1,
-    #                                maxRadius=40)
+# opens the file to splinter the video
+import framesSplit
