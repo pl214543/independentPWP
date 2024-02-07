dd1486853d2d8f5bc209e4fd5f4dc09bdd70f9c4
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-02-07 08:37:34 -0500

    Update main.py

--- paperLanes/main.py
+++ paperLanes/main.py
@@ -3,6 +3,11 @@
 # https://www.geeksforgeeks.org/python-opencv-cv2-line-method/
 # cv2.line(image, start_point, end_point, color, thickness)
 # have a programming plan and glossary + technical algorithm
+# dodge and burn
+# field of view
+# maybe grayscale and then remove all darker colors (+ dodge and burn)
+# https://www.geeksforgeeks.org/filter-color-with-opencv/
+# https://www.geeksforgeeks.org/python-opencv-capture-video-from-camera/ for capturing video and editing
 
 # import required libraries
 import cv2
@@ -16,4 +21,25 @@ def checkVideo(video):
 
 checkVideo(video)
 
-import detectLines
+# https://www.geeksforgeeks.org/python-opencv-capture-video-from-camera/
+while True:
+    booleanReady, frame = video.read()
+
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(grey, (1, 1), 0)
+    # canny = cv2.Canny(blurred, 100, 200)
+
+    # https://www.geeksforgeeks.org/filter-color-with-opencv/
+    ranged = cv2.inRange(blurred, 125, 255)
+    result = cv2.bitwise_and(blurred, blurred, mask=ranged)
+
+    cv2.imshow('Frame', result)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
+
+# import detectLines
+
